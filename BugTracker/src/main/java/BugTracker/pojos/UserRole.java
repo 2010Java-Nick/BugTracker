8e9ec13c94409a4b4c3d58437891aa1b33af95d4
Ksenia <kxmilste@oaklandcc.edu>
2020-12-10 15:04:44 -0500

    Added dependencies to gradle.build and created ServerDriver and started creating pojos

---
+++ BugTracker/src/main/java/BugTracker/pojos/UserRole.java
@@ -0,0 +1,84 @@
+package BugTracker.pojos;
+
+public class UserRole {
+	
+	private long roleId;
+	private String roleName;
+	private int power;
+	public UserRole() {
+		super();
+	}
+	
+	public UserRole(String roleName, int power) {
+		super();
+		this.roleName = roleName;
+		this.power = power;
+	}
+
+	public UserRole(long roleId, String roleName, int power) {
+		super();
+		this.roleId = roleId;
+		this.roleName = roleName;
+		this.power = power;
+	}
+
+	public long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + power;
+		result = prime * result + (int) (roleId ^ (roleId >>> 32));
+		result = prime * result + ((roleName == null) ? 0 : roleName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserRole other = (UserRole) obj;
+		if (power != other.power)
+			return false;
+		if (roleId != other.roleId)
+			return false;
+		if (roleName == null) {
+			if (other.roleName != null)
+				return false;
+		} else if (!roleName.equals(other.roleName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "UserRole [roleId=" + roleId + ", roleName=" + roleName + ", power=" + power + "]";
+	}
+}
