fc4250ab3c4440ffffd0af4a92f64d6a1027f5ac
aholliday <acacia.holliday@snhu.edu>
2020-12-12 19:30:22 -0500

    working on createTicket dao method

--- BugTracker/src/test/java/DaoTests/TicketDaoTest.java
+++ BugTracker/src/test/java/DaoTests/TicketDaoTest.java
@@ -3,13 +3,20 @@ package DaoTests;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
+import java.time.LocalDateTime;
 
 import org.hibernate.SessionFactory;
 import org.mockito.InjectMocks;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import BugTracker.dao.TicketDaoImpl;
+import BugTracker.pojos.Employee;
+import BugTracker.pojos.Priority;
+import BugTracker.pojos.Status;
 import BugTracker.pojos.Ticket;
+import BugTracker.pojos.UserRole;
+import BugTracker.utils.SessionFactoryUtil;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -21,11 +28,11 @@ import org.junit.runner.RunWith;
 @RunWith(MockitoJUnitRunner.class)
 public class TicketDaoTest {
 	
-	@InjectMocks
-	public SessionFactory sessionFactory;
+	@Autowired
+	public SessionFactoryUtil sessionFactoryUtil;
 	
 
-	public TicketDaoImpl ticketDao = new TicketDaoImpl(sessionFactory);
+	public TicketDaoImpl ticketDao = new TicketDaoImpl(sessionFactoryUtil.getSessionFactoryUtil().getSessionFactory());
 	
 	
 
@@ -54,7 +61,14 @@ public class TicketDaoTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		UserRole developer = new UserRole("Developer", 2);
+		Priority priority1 = new Priority("Urgent");
+		Status opened = new Status("Opened");
+		Employee employee1 = new Employee("Acacia", "Holliday", "aholliday@gmail.com", 0, developer);
+		Employee employee2 = new Employee("Hannah", "Novack", "hNovack@gmail.com", 0, developer);
+		Ticket ticket = new Ticket(employee1, "Test ticket", LocalDateTime.now(),opened, priority1, 3, null, employee2);
+		Ticket returnedTicket = ticketDao.createTicket(ticket);
+		assertEquals(returnedTicket, ticket);
 	}
 
 }
