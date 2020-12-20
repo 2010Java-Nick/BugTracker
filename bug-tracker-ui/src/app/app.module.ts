7756508620d9ce1b37745b5c0f0b86a0b29ae390
Hannah Novack <hmnov@umich.edu>
2020-12-19 19:31:20 -0500

    ts models

--- bug-tracker-ui/src/app/app.module.ts
+++ bug-tracker-ui/src/app/app.module.ts
@@ -3,26 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TicketFormComponent } from './components/ticket-form/ticket-form.component';
+import { TicketFormComponent } from './ticket/ticket-form/ticket-form.component';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { OpenedComponent } from './components/opened/opened.component';
-import { AssignedComponent } from './components/assigned/assigned.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TicketFormComponent,
-    DashboardComponent,
-    OpenedComponent,
-    AssignedComponent
+    TicketFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
