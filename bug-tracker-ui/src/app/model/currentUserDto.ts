e8253c3e5d7c9263176b46b4fff45f2774c60d63
JoeDKoal94 <joedkoal94@gmail.com>
2020-12-20 15:31:51 -0600

    here

---
+++ bug-tracker-ui/src/app/model/currentUserDto.ts
@@ -0,0 +1,8 @@
+export class CurrentUser{
+  
+    constructor(
+        public id: number,
+        public email: String,
+        public role: String
+    ) {}
+}
---
+++ bug-tracker-ui/src/app/model/ticketDtoDisplay.ts
@@ -0,0 +1,9 @@
+export class TicketDisplay {
+    constructor(
+        public openerName: String,
+        public name: String,
+        public priorityId: number,
+        public difficultyLevel: number,
+        public assignedName: String
+    ){ }
+}
