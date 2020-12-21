43b2c81bf654f949712b26ecdeb4fdd02638c567
Hannah Novack <hmnov@umich.edu>
2020-12-20 23:01:30 -0500

    update ticket in opened component

--- bug-tracker-ui/src/app/components/opened/opened.component.ts
+++ bug-tracker-ui/src/app/components/opened/opened.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { TicketDto } from 'src/app/model/ticket';
 import { TicketDisplay } from 'src/app/model/ticketDtoDisplay';
 import { TicketServiceService } from '../ticket/ticket-service.service';
 
@@ -11,11 +13,21 @@ export class OpenedComponent implements OnInit {
 
   allTickets!: TicketDisplay[];
 
-  constructor(private ticketService: TicketServiceService) { }
+  ticketDisplay!: TicketDisplay;
+
+  submitted = true;
+
+  
+
+  constructor(private ticketService: TicketServiceService) { 
+  }
 
   ngOnInit(): void {
       this.ticketService.getOpenerTickets().subscribe(data => {this.allTickets = data, console.log(this.allTickets)});
-      
+  }
+
+  updateTicket(ticketDisplay: TicketDisplay): void {
+    this.ticketService.updateTicket(ticketDisplay).subscribe(data => {this.ticketDisplay = data, console.log(this.ticketDisplay)});
   }
 
 }
